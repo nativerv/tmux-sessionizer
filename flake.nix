fb20a845132c0fb6015f92b3a624560bd5ee7e9f
nativerv <nativerv@protonmail.com>
2024-02-09 20:59:57 +0300

    build: add overlay to the flake

--- flake.nix
+++ flake.nix
@@ -7,6 +7,9 @@
   outputs = { nixpkgs, systems, ... }: let
     forEachSystem = nixpkgs.lib.genAttrs (import systems);
   in {
+    overlays.default = final: prev: {
+      tmux-sessionizer = final.callPackage ./tmux-sessionizer.nix {};
+    };
     packages = forEachSystem (system: {
       default = nixpkgs.legacyPackages.${system}.callPackage ./tmux-sessionizer.nix {};
     });
